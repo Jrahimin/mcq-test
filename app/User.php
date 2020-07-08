565c7e5c014973fef5058d09058c38c35f361712
Junayed Rahimin <rahiminmyself@gmail.com>
2020-07-09 00:52:10 +0600

    user store update

--- app/User.php
+++ app/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email','mobile_no','address','type','status'
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for arrays.
