34a9ba2cd1d48ddf8cd681eabb57168d929853d8
Md Rana Hossain <rana.hossain@sslwireless.com>
2020-07-24 00:51:05 +0600

    exam preview front-end done

--- app/Http/Controllers/Frontend/UserMcqTestController.php
+++ app/Http/Controllers/Frontend/UserMcqTestController.php
@@ -29,20 +29,20 @@ class UserMcqTestController extends Controller
 
     public function generateExamQuestion(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'exam_id' => 'required'
         ]);
 
-        try{
+        try {
             $exam = ExamTest::find($request->exam_id);
             $data = $this->data;
             $data['exam_test_id'] = $request->exam_id;
 
-            if(!$request->wantsJson()){
+            if (!$request->wantsJson()) {
                 return view('frontend.mcq-test', $data);
             }
             $query = $exam->questions()->with('activeAnswers')->where('status', 1);
-            if($request->subject_id){
+            if ($request->subject_id) {
                 $query = $query->where('subject_id', $request->subject_id);
             }
 
@@ -51,25 +51,23 @@ class UserMcqTestController extends Controller
             $questionPaper = [];
             $questionPaper['examInfo'] = array(
                 'title' => $exam->title,
-                'duration_sec' => $exam->duration_minutes*60,
+                'duration_sec' => $exam->duration_minutes * 60,
                 'mark_per_question' => $exam->mark_per_question,
                 'question_count' => count($questionList)
             );
-            foreach ($questionList as $question)
-            {
+            foreach ($questionList as $question) {
                 $optionList = [];
-                foreach ($question->activeAnswers as $option)
-                {
+                foreach ($question->activeAnswers as $option) {
                     $optionList[] = array(
-                        'option'     => $option->answer,
-                        'option_id'  => $option->id
+                        'option' => $option->answer,
+                        'option_id' => $option->id
                     );
                 }
 
                 $questionPaper['questionList'][] = array(
-                    'question'    => $question->title,
+                    'question' => $question->title,
                     'question_id' => $question->id,
-                    'mark'        => $question->mark,
+                    'mark' => $question->mark,
                     'description' => $question->description,
 
                     'options' => $optionList
@@ -77,8 +75,7 @@ class UserMcqTestController extends Controller
             }
 
             return $this->successResponse('Question paper fetched', $questionPaper);
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             Log::error('[Class => ' . __CLASS__ . ", function => " . __FUNCTION__ . " ]" . " @ " . $ex->getFile() . " " . $ex->getLine() . " " . $ex->getMessage());
             return $this->exceptionResponse($this->exceptionMessage);
         }
@@ -86,7 +83,7 @@ class UserMcqTestController extends Controller
 
     public function submit(Request $request)
     {
-        try{
+        try {
             $exam = ExamTest::findOrFail($request->exam_id);
 
             $questions = $exam->questions()->where('status', 1)
@@ -94,13 +91,11 @@ class UserMcqTestController extends Controller
 
             // Generate AnswerList -- Format --> list['question_id' => correctAnswerId]
             $answerList = [];
-            foreach ($questions as $question)
-            {
+            foreach ($questions as $question) {
                 $correctAnswer = $question->activeAnswers->where('is_correct', 1)->first();
                 $answerList[$question->id] = $correctAnswer->id;
             }
-
-            Log::info("exam answer list : ".json_encode($answerList));
+            Log::info("exam answer list : " . json_encode($answerList));
 
             // Generate User provided AnswerList -- Format --> list['question_id' => userGivenAnswerId]
             $userAnswerList = [];
@@ -108,53 +103,50 @@ class UserMcqTestController extends Controller
             $attainedMark = 0;
             $noOfCorrectAnswer = 0;
             $noOfWrongAnswer = 0;
-            foreach ($request->answers as $answer)
-            {
+            foreach ($request->answers as $answer) {
                 $userAnswerList[$answer['question_id']] = $answer['option_id'];
 
-                if($answerList[$answer['question_id']] == $answer['option_id']){    //if answerList correctAns matches user provided option_id -- mark++
+                if ($answerList[$answer['question_id']] == $answer['option_id']) {    //if answerList correctAns matches user provided option_id -- mark++
                     $attainedMark++;
                     $noOfCorrectAnswer++;
-                } else{
+                } else {
                     $attainedMark -= $exam->negative_mark_per_question;
                     $noOfWrongAnswer++;
                 }
             }
 
-            Log::info("user provided answer list : ".json_encode($userAnswerList));
+            Log::info("user provided answer list : " . json_encode($userAnswerList));
 
             // Generate preview of exam paper
             $examPaperReview = [];
             $examPaperReview['examInfo'] = array(
                 'title' => $exam->title,
-                'duration_sec' => $exam->duration_minutes*60,
+                'duration_sec' => $exam->duration_minutes * 60,
                 'mark_per_question' => $exam->mark_per_question,
                 'question_count' => $totalMark,
                 'total_mark' => $totalMark,
-                'user_mark'  => $attainedMark,
+                'user_mark' => $attainedMark,
                 'correct_answer_count' => $noOfCorrectAnswer,
                 'wrong_answer_count' => $noOfWrongAnswer
             );
 
-            foreach ($questions as $question)
-            {
+            foreach ($questions as $question) {
                 $optionList = [];
-                foreach ($question->activeAnswers as $option)
-                {
+                foreach ($question->activeAnswers as $option) {
                     $optionList[] = array(
-                        'option'     => $option->answer,
-                        'option_id'  => $option->id
+                        'option' => $option->answer,
+                        'option_id' => $option->id
                     );
                 }
 
                 $userOptionId = array_key_exists($question->id, $userAnswerList) ? $userAnswerList[$question->id] : null;
                 $correctOptionId = $answerList[$question->id];
                 $examPaperReview['questionList'][] = array(
-                    'question'          => $question->title,
-                    'question_id'       => $question->id,
-                    'mark'              => $question->mark,
-                    'description'       => $question->description,
-                    'user_option_id'    => array_key_exists($question->id, $userAnswerList) ? $userAnswerList[$question->id] : null,
+                    'question' => $question->title,
+                    'question_id' => $question->id,
+                    'mark' => $question->mark,
+                    'description' => $question->description,
+                    'user_option_id' => array_key_exists($question->id, $userAnswerList) ? $userAnswerList[$question->id] : null,
                     'correct_option_id' => $answerList[$question->id],
                     'is_user_correct'   => (bool) ($userOptionId && $userOptionId == $correctOptionId),
 
@@ -162,14 +154,13 @@ class UserMcqTestController extends Controller
                 );
             }
 
-            Log::info("Exam paper review : ".json_encode($examPaperReview));
+            Log::info("Exam paper review : " . json_encode($examPaperReview));
 
             DB::table('exam_test_user')->where('user_id', $request->user()->id)->where('exam_test_id', $exam->id)
                 ->update(['score' => $attainedMark, 'total_correct' => $attainedMark, 'total_wrong' => $totalMark - $attainedMark]);
 
             return $this->successResponse('Exam result preview', $examPaperReview);
-        }
-        catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             Log::error('[Class => ' . __CLASS__ . ", function => " . __FUNCTION__ . " ]" . " @ " . $ex->getFile() . " " . $ex->getLine() . " " . $ex->getMessage());
             abort(500);
         }
--- resources/views/frontend/mcq-test.blade.php
+++ resources/views/frontend/mcq-test.blade.php
@@ -407,7 +407,7 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid bg-light" style="min-height: 725px">
+        <div class="container-fluid bg-light" v-if="!is_exam_completed" style="min-height: 725px">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -472,6 +472,82 @@
                 </div>
             </div>
         </div>
+        <div v-if="is_exam_completed" class="container-fluid" style="margin-top: 3%">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h3 class="text-center">Exam Review</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <span class="badge">@{{ exam_info_response.question_count }}</span>
+                            Number of Question
+                        </li>
+                        <li class="list-group-item">
+                            <span class="badge">@{{ exam_info_response.total_mark }}</span>
+                            Total Mark
+                        </li>
+                        <li class="list-group-item">
+                            <span class="badge badge-info" style="background-color: #12badc">@{{ exam_info_response.user_mark }}</span>
+                            Your Mark
+                        </li>
+                        <li class="list-group-item">
+                            <span class="badge badge-primary" style="background-color: rgba(23,196,49,0.62)">@{{ exam_info_response.correct_answer_count }}</span>
+                            Correct Answer
+                        </li>
+                        <li class="list-group-item">
+                            <span class="badge badge-danger" style="background-color: rgba(255,0,0,0.66)">@{{ exam_info_response.wrong_answer_count }}</span>
+                            Wrong Answer
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-5 col-md-offset-1" style="padding: 2%; background-color: rgba(195,205,205,0.2)"
+                     v-for="question_response of question_list_response">
+                    <div class="accordion md-accordion accordion-blocks"
+                         aria-multiselectable="true">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="mt-1 mb-0" style="color: #00b44e; padding-left: 2%"
+                                    v-if="question_response.is_user_correct">
+                                    <i class="glyphicon glyphicon-ok"></i>
+                                    @{{ question_response.question }}
+                                </h5>
+                                <h5 class="mt-1 mb-0" v-if="!question_response.is_user_correct"
+                                    style="color: red; padding-left: 2%">
+                                    <i class="glyphicon glyphicon-remove"></i>
+                                    @{{ question_response.question }}
+                                </h5>
+                            </div>
+                            <div class="card-body">
+                                <ul v-if="question_response.user_option_id == question_response.correct_option_id">
+                                    <li v-for="option of question_response.options"
+                                        :style="{'background':question_response.user_option_id == option.option_id?'rgba(0,180,78,0.46)':'',margin:'1%'}">
+                                        @{{ option.option }}
+                                        <span v-if="question_response.user_option_id == option.option_id"
+                                              class="pull-right" style="padding-right: 1%;color: green"><i
+                                                class="glyphicon glyphicon-ok"></i></span>
+                                    </li>
+                                </ul>
+                                <ul v-else>
+                                    <li v-for="option of question_response.options"
+                                        :style="{'background':question_response.user_option_id == option.option_id?'rgba(239,10,41,0.53)':question_response.correct_option_id == option.option_id?'rgba(0,180,78,0.46)':'',margin:'1%'}">
+                                        @{{ option.option }}
+                                        <span v-if="question_response.correct_option_id == option.option_id"
+                                              class="pull-right" style="padding-right: 1%; color: #00b44e"><i
+                                                class="glyphicon glyphicon-ok"></i></span>
+                                        <span v-if="question_response.user_option_id == option.option_id"
+                                              class="pull-right" style="padding-right: 1%; color: red"><i
+                                                class="glyphicon glyphicon-remove"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-padding"></div>
+        </div>
     </div>
 @endsection
 @section('script-lib')
@@ -491,6 +567,9 @@
                 question_count: '',
                 questions: [],
                 answers: [],
+                is_exam_completed: false,
+                question_list_response: [],
+                exam_info_response: {},
             },
             methods: {
                 ajaxCall: window.ajaxCall,
@@ -520,7 +599,9 @@
                         "answers": this.answers,
                     }, 'post', (data, code) => {
                         if (code === 200) {
-                            window.location.href = data.redirect_url;
+                            this.is_exam_completed = true;
+                            this.question_list_response = data.questionList || [];
+                            this.exam_info_response = data.examInfo || {};
                         } else {
                             sweetAlert('Error', "Something went wrong! please contact with A2B Publication", 'error');
                         }
@@ -561,6 +642,7 @@
                         this.secLeft = 0;
                         clearInterval(interval);
                         sweetAlert('Fail!', 'Time over', 'warning');
+                        this.answerSubmit()
                     }
                     this.secLeft -= intervalUnit;
                     this.percentOfTimeProgress = 100 - (this.secLeft * 100 / this.second);
