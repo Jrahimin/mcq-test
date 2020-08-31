eb0ea94567bf64fdfb81d18f72a1310358680184
Junayed Rahimin <rahiminmyself@gmail.com>
2020-09-01 03:34:38 +0600

    update

--- resources/views/layouts/frontend/header-top.blade.php
+++ resources/views/layouts/frontend/header-top.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
             <div class="topheader-left">
                 <a href="tel:01816804737" title="01816804737"><i class="fa fa-mobile" aria-hidden="true"></i>01816804737</a>
-                <a href="mailto:admin@a2bpublications.com" title="admin@a2bpublications.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>Emailus: admin@a2bpublications.com</a>
+                <a href="mailto:admin@a2bpublications.com" title="admin@a2bpublications.com"><i class="fa fa-envelope-o" aria-hidden="true"></i>admin@a2bpublications.com</a>
             </div>
             <div class="topheader-right">
                 <a href="{{route('login')}}" title="Login"><i class="fa fa-sign-out"
