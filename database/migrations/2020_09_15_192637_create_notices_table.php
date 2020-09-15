d20dd9f9307a11307d198ba767b5c7dbd4e6f97f
Junayed Rahimin <rahiminmyself@gmail.com>
2020-09-15 22:04:44 +0600

    notica, article model+migration

---
+++ database/migrations/2020_09_15_192637_create_notices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->date('date_from')->nullable();
+            $table->date('date_to')->nullable();
+            $table->string('image_url')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
